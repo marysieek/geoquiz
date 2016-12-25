34c5c233496e40a537bf294d378f6fcf81fa0285
Maria Stokłosa <maria.stoklosa@netguru.pl>
2016-12-25 21:45:06 +0100

    Cleanup code - linter

--- app/src/main/java/pl/korlotian/geoquiz/Question.java
+++ app/src/main/java/pl/korlotian/geoquiz/Question.java
@@ -1,9 +1,5 @@
 package pl.korlotian.geoquiz;
 
-/**
- * Created by maria on 03/12/16.
- */
-
 public class Question {
 
     private int mTextResId;
@@ -18,15 +14,8 @@ public class Question {
         return mAnswerTrue;
     }
 
-    public void setAnswerTrue(boolean answerTrue) {
-        mAnswerTrue = answerTrue;
-    }
-
     public int getTextResId() {
         return mTextResId;
     }
 
-    public void setTextResId(int textResId) {
-        mTextResId = textResId;
-    }
 }
